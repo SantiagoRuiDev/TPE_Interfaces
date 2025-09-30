4948b640443fc854e4ce9325d9fa1f5adf08a9fe
Santiago <117700156+SantiagoRuiDev@users.noreply.github.com>
2025-09-30 11:05:43 -0300

    Se agrego el selector de categorias para el home

--- Entrega2/js/catalog.js
+++ Entrega2/js/catalog.js
@@ -1,3 +1,4 @@
+import { mapCategoryButton } from "./button/category_button.js";
 import { mapDiscountCard } from "./card/discount_card.js";
 import { mapFreeCard } from "./card/free_card.js";
 import { mapNormalCard } from "./card/normal_card.js";
@@ -5,57 +6,76 @@ import { mapOwnedCard } from "./card/owned_card.js";
 import { mapProductCartCard } from "./card/product_cart_card.js";
 
 let cart = [];
-const catalog = document.querySelector(".game-list");
+let categories = [];
+const personal_catalog = document.querySelector("#personal-interest-games");
+const category_selector = document.querySelector(".category-options");
 const next_btn = document.querySelector("#next-btn");
 const prev_btn = document.querySelector("#prev-btn");
-const catalog_items = [];
+const personal_catalog_items = [];
 const items_per_page = 4;
 let currentPage = 0;
 
 next_btn.addEventListener("click", () => {
   const nextPage =
-    currentPage + 1 < catalog_items.length ? currentPage + 1 : currentPage;
+    currentPage + 1 < personal_catalog_items.length ? currentPage + 1 : currentPage;
   currentPage = nextPage;
-  renderCatalogPage(currentPage);
+  renderpersonal_catalogPage(currentPage);
 });
 
 prev_btn.addEventListener("click", () => {
   const previousPage = currentPage - 1 >= 0 ? currentPage - 1 : currentPage;
   currentPage = previousPage;
-  renderCatalogPage(currentPage);
+  renderpersonal_catalogPage(currentPage);
 });
 
 // En base a la pagina seleccionada elimina los items listados y carga los nuevos.
-const renderCatalogPage = (page) => {
+const renderpersonal_catalogPage = (page) => {
   currentPage = page;
-  catalog.innerHTML = ""; // Vaciar el catalogo
-  for (const game of catalog_items[page]) {
+  personal_catalog.innerHTML = ""; // Vaciar el personal_catalogo
+  for (const game of personal_catalog_items[page]) {
     if (game.discount) {
-      catalog.innerHTML += mapDiscountCard(game);
+      personal_catalog.innerHTML += mapDiscountCard(game);
     } else if (game.is_free) {
-      catalog.innerHTML += mapFreeCard(game);
+      personal_catalog.innerHTML += mapFreeCard(game);
     } else if (game.is_owned) {
-      catalog.innerHTML += mapOwnedCard(game);
+      personal_catalog.innerHTML += mapOwnedCard(game);
     } else {
-      catalog.innerHTML += mapNormalCard(game);
+      personal_catalog.innerHTML += mapNormalCard(game);
     }
   }
 
-  // Esta función se dispara cuando se termina de cargar el catalogo, si se hace antes capturara una lista vacia.
+  // Esta función se dispara cuando se termina de cargar el personal_catalogo, si se hace antes capturara una lista vacia.
   enableCartListing();
 };
 
-// Esta función captura el evento click para cada boton de acción de las cards del catalogo.
+const renderCategorySelector = () => {
+  for(const category of categories){
+    category_selector.innerHTML += mapCategoryButton(category);
+  }
+  listenCategorySelector(); // Escuchar cambios de categoria.
+}
+
+// Esta función captura el evento click para cada boton de acción de las cards del personal_catalogo.
 // Entonces nos permite saber cuando poner un elemento en el carrito.
 const enableCartListing = () => {
   const buy_action_button = document.querySelectorAll(".action-button");
   buy_action_button.forEach((btn, index) => {
     btn.addEventListener("click", () => {
-      const product = catalog_items[currentPage][index];
+      const product = personal_catalog_items[currentPage][index];
       addProductToCart(product);
     });
   });
 };
+
+const listenCategorySelector = () => {
+  const category_buttons = document.querySelectorAll(".category-action-button");
+  category_buttons.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      category_buttons.forEach((b) => b.classList.remove("active"));
+      btn.classList.toggle('active');
+    });
+  });
+}
 
 // Se agrega el producto al carrito y se renderiza el HTML.
 const addProductToCart = (product) => {
@@ -110,8 +130,8 @@ const deleteProductFromCart = (product) => {
 
 /**
  *
- * Función inicial, consulta a la API y obtiene resultados para mapear catalogo de Juegos
- * (Los juegos mostrados en el catalogo pueden ser diferentes por sugerencias, generales o adquiridos)
+ * Función inicial, consulta a la API y obtiene resultados para mapear personal_catalogo de Juegos
+ * (Los juegos mostrados en el personal_catalogo pueden ser diferentes por sugerencias, generales o adquiridos)
  *
  */
 const fetchCatalogItems = () => {
@@ -121,15 +141,19 @@ const fetchCatalogItems = () => {
       .then((data) => {
         let aux_row = [];
         for (const i of data) {
+          if(categories.length == 0 || !categories.includes(i.category)){
+            categories.push(i.category);
+          }
           if (aux_row.length < items_per_page) {
             aux_row.push(i);
           } else {
-            catalog_items.push(aux_row);
+            personal_catalog_items.push(aux_row);
             aux_row = [];
             aux_row.push(i);
           }
         }
-        renderCatalogPage(currentPage);
+        renderCategorySelector();
+        renderpersonal_catalogPage(currentPage);
       })
       .catch((err) => console.log(err));
   } catch (error) {}
